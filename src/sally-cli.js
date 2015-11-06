fe984da76023d30738ed22a09c26206f1e441848
Richard Schneider <makaretu@gmail.com>
2015-11-06 23:13:59 +1300

    Allow glob (wildcards) for sally verify <file>

--- src/sally-cli.js
+++ src/sally-cli.js
@@ -11,6 +11,7 @@
 
 var program = require('commander');
 var process = require('process');
+var Glob = require("glob").Glob
 var fs = require('fs');
 var reader = require('./reader');
 
@@ -23,28 +24,52 @@ program
   
 program
   .command('verify <file>')
-  .description('verify the audit trail file')
+  .description('verify the audit trail file, glob wildcards allowed')
   .action(verify)
 
 program.parse(process.argv);
+if (!program.args.length) program.help();
 
-function verify(file, options) {
-    var secret = program.secret || process.env.sallySecret;
-	if (!secret) {
-		console.error('Need the audit trail\'s secret; use --secret [text]');
-		return 1;
-	}
-	new reader({path: file, secret: secret})
-		.createReadStream()
-		.on('data', function (entry) {
-			if (program.verbose)
-				console.log(entry);
+function verify(pattern, options) {
+	var didSomething = false;
+	var glob = new Glob(pattern, { strict: true })
+	glob
+		.on('match', function(path) {
+			didSomething = true;
+			verifyFile(path, options);
 		})
 		.on('error', function (e) {
 			console.error(e.message)
-			process.exit(2)
+			process.exitCode = 1;
 		})
 		.on('end', function () {
-			process.exit(0);
+			if (!didSomething) {
+				console.error('No files were processed')
+				process.exitCode = 1;
+			}
+		});
+}
+
+function verifyFile(path, options) {
+    var secret = program.secret || process.env.sallySecret;
+	if (!secret) {
+		console.error('Need the audit trail\'s secret; use --secret [text]');
+		process.exitCode = 1;
+		return;
+	}
+	if (program.verbose)
+		console.log("Verifying", path);
+	
+	new reader({path: path, secret: secret})
+		.createReadStream()
+		.on('data', function (entry) {
+		})
+		.on('error', function (e) {
+			console.error(e.message)
+			process.exitCode = 1;
+		})
+		.on('end', function () {
+			if (program.verbose)
+				console.log("Passed", path);
 		});
 }
